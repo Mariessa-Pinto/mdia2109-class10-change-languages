ce5b8b004afe2010b2d90d929fdd8b97419f177d
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-03-06 21:06:20 -0800

    added footer component

---
+++ components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+import Image from 'next/image'
+import { FormattedMessage } from 'react-intl'
+
+export default function Footer() {
+    return (
+        <div>
+
+        </div>
+    )
+}
