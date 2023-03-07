2a12571eb39e3a870c3c2f7e2ec4d82949144cbd
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-03-06 21:07:06 -0800

    added sidebar component

---
+++ components/Sidebar/index.js
@@ -0,0 +1,11 @@
+import styles from './Sidebar.module.css'
+import Image from 'next/image'
+import { FormattedMessage } from 'react-intl'
+
+export default function Sidebar() {
+    return (
+        <div>
+
+        </div>
+    )
+}
