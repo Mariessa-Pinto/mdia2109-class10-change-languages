e1052c413f2ec5680b599988458332c932ce685b
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-03-07 19:28:50 -0800

    Styled the first half of the sidebar

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -5,46 +5,48 @@ export default function Sidebar() {
     return (
         <div className={styles.sidebar}>
             <div className={styles.about}>
-                <h4><FormattedMessage id="page.home.sidebar.about.header"/></h4>
-                <ul>
-                    <li><FormattedMessage id="page.home.sidebar.about.item.one"/></li>
-                    <li><FormattedMessage id="page.home.sidebar.about.item.two"/></li>
-                    <li><FormattedMessage id="page.home.sidebar.about.item.three"/></li>
-                    <li><FormattedMessage id="page.home.sidebar.about.item.four"/></li>
-                    <li><FormattedMessage id="page.home.sidebar.about.item.five"/></li>
-                    <li><FormattedMessage id="page.home.sidebar.about.item.six"/></li>
-                    <li><FormattedMessage id="page.home.sidebar.about.item.seven"/></li>
-                    <li><FormattedMessage id="page.home.sidebar.about.item.eight"/></li>
-                    <li><FormattedMessage id="page.home.sidebar.about.item.nine"/></li>
+                <div className={styles.headerBox}>
+                    <h4 className={styles.header}><FormattedMessage id="page.home.sidebar.about.header"/></h4>
+                </div>
+                <ul className={styles.aboutList}>
+                    <li className={styles.aboutListItem}><FormattedMessage id="page.home.sidebar.about.item.one"/></li>
+                    <li className={styles.aboutListItem}><FormattedMessage id="page.home.sidebar.about.item.two"/></li>
+                    <li className={styles.aboutListItem}><FormattedMessage id="page.home.sidebar.about.item.three"/></li>
+                    <li className={styles.aboutListItem}><FormattedMessage id="page.home.sidebar.about.item.four"/></li>
+                    <li className={styles.aboutListItem}><FormattedMessage id="page.home.sidebar.about.item.five"/></li>
+                    <li className={styles.aboutListItem}><FormattedMessage id="page.home.sidebar.about.item.six"/></li>
+                    <li className={styles.aboutListItem}><FormattedMessage id="page.home.sidebar.about.item.seven"/></li>
+                    <li className={styles.aboutListItem}><FormattedMessage id="page.home.sidebar.about.item.eight"/></li>
+                    <li className={styles.aboutListItem}><FormattedMessage id="page.home.sidebar.about.item.nine"/></li>
                 </ul>
             </div>
             <div className={styles.info}>
-                <h3><FormattedMessage id="page.home.sidebar.info.header"/></h3>
-                <p><FormattedMessage id="page.home.sidebar.info.desc"/></p>
-                <ul>
-                    <li><FormattedMessage id="page.home.sidebar.info.list.one"/></li>
-                    <li><FormattedMessage id="page.home.sidebar.info.list.two"/></li>
-                    <li><FormattedMessage id="page.home.sidebar.info.list.three"/></li>
-                    <li><FormattedMessage id="page.home.sidebar.info.list.four"/></li>
+                <h3 className={styles.headerInfo}><FormattedMessage id="page.home.sidebar.info.header"/></h3>
+                <p className={styles.infoText}><FormattedMessage id="page.home.sidebar.info.desc"/></p>
+                <ul className={styles.infoList}>
+                    <li className={styles.dates}><FormattedMessage id="page.home.sidebar.info.list.one"/></li>
+                    <li className={styles.dates}><FormattedMessage id="page.home.sidebar.info.list.two"/></li>
+                    <li className={styles.dates}><FormattedMessage id="page.home.sidebar.info.list.three"/></li>
+                    <li className={styles.dates}><FormattedMessage id="page.home.sidebar.info.list.four"/></li>
                 </ul>
-                <p><FormattedMessage id="page.home.sidebar.info.explore"/></p>
+                <p className={styles.infoText}><FormattedMessage id="page.home.sidebar.info.explore"/></p>
             </div>
             <div className={styles.overview}>
-                <h2><FormattedMessage id="page.home.sidebar.overview.header"/></h2>
+                <h2 className={styles.header}><FormattedMessage id="page.home.sidebar.overview.header"/></h2>
                 <ul>
-                    <li><FormattedMessage id="page.home.sidebar.overview.credential"/></li>
-                    <li><FormattedMessage id="page.home.sidebar.overview.format"/></li>
-                    <li><FormattedMessage id="page.home.sidebar.overview.length"/></li>
-                    <li><FormattedMessage id="page.home.sidebar.overview.start"/></li>
-                    <li><FormattedMessage id="page.home.sidebar.overview.campus"/></li>
-                    <li><FormattedMessage id="page.home.sidebar.overview.domestic"/></li>
-                    <li><FormattedMessage id="page.home.sidebar.overview.int"/></li>
-                    <li><FormattedMessage id="page.home.sidebar.overview.cost"/></li>
+                    <li className={styles.overviewList}><FormattedMessage id="page.home.sidebar.overview.credential"/></li>
+                    <li className={styles.overviewList}><FormattedMessage id="page.home.sidebar.overview.format"/></li>
+                    <li className={styles.overviewList}><FormattedMessage id="page.home.sidebar.overview.length"/></li>
+                    <li className={styles.overviewList}><FormattedMessage id="page.home.sidebar.overview.start"/></li>
+                    <li className={styles.overviewList}><FormattedMessage id="page.home.sidebar.overview.campus"/></li>
+                    <li className={styles.overviewList}><FormattedMessage id="page.home.sidebar.overview.domestic"/></li>
+                    <li className={styles.overviewList}><FormattedMessage id="page.home.sidebar.overview.int"/></li>
+                    <li className={styles.overviewList}><FormattedMessage id="page.home.sidebar.overview.cost"/></li>
                 </ul>
-                <h5><FormattedMessage id="page.home.sidebar.overview.link"/></h5>
+                <h5 className={styles.allPrograms}><FormattedMessage id="page.home.sidebar.overview.link"/></h5>
             </div>
             <div className={styles.recent}>
-                <h2><FormattedMessage id="page.home.sidebar.recent.header"/></h2>
+                <h2 className={styles.header}><FormattedMessage id="page.home.sidebar.recent.header"/></h2>
                 <p><FormattedMessage id="page.home.sidebar.recent.link.one"/></p>
                 <p><FormattedMessage id="page.home.sidebar.recent.link.two"/></p>
             </div>
