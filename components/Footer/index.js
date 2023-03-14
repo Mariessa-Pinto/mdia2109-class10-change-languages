e10832aa83b95c7ea932708d30a297b755288dc3
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-03-13 19:16:19 -0700

    stylized footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -7,16 +7,19 @@ export default function Footer() {
         <div className={styles.footer}>
             <div className={styles.colOne}>
                 <div className={styles.topSec}>
-                    <h4><FormattedMessage id="page.home.footer.col.one.header"/></h4>
-                    <p><FormattedMessage id="page.home.footer.col.one.address"/></p>
-                    <p><FormattedMessage id="page.home.footer.col.one.city"/></p>
-                    <p><FormattedMessage id="page.home.footer.col.one.coutry"/></p>
-                    <p><FormattedMessage id="page.home.footer.col.one.web"/></p>
+                    <div className={styles.headerWithLine}>
+                        <div className={styles.line}></div>
+                        <h4><FormattedMessage id="page.home.footer.col.one.header"/></h4>
+                    </div>
+                    <p className={styles.address}><FormattedMessage id="page.home.footer.col.one.address"/></p>
+                    <p className={styles.address}><FormattedMessage id="page.home.footer.col.one.city"/></p>
+                    <p className={styles.address}><FormattedMessage id="page.home.footer.col.one.country"/></p>
+                    <p className={styles.bcitSite}><FormattedMessage id="page.home.footer.col.one.web"/></p>
                 </div>
                 <div className={styles.bottomSec}>
-                    <p><FormattedMessage id="page.home.footer.col.one.tel.header"/> 604-434-5734</p>
-                    <p><FormattedMessage id="page.home.footer.col.one.toll.free"/> 1-866-434-1610</p>
-                    <p><FormattedMessage id="page.home.footer.col.one.more"/></p>
+                    <p className={styles.address}><FormattedMessage id="page.home.footer.col.one.tel.header"/><span className={styles.blue}> 604-434-5734</span></p>
+                    <p className={styles.address}><FormattedMessage id="page.home.footer.col.one.toll.free"/><span className={styles.blue}> 1-866-434-1610</span></p>
+                    <p className={styles.contact}><FormattedMessage id="page.home.footer.col.one.more"/></p>
                 </div>
                 <div className={styles.socials}>
                 <Image
@@ -50,22 +53,25 @@ export default function Footer() {
                 </div>
             </div>
             <div className={styles.colTwo}>
-                <p><FormattedMessage id="page.home.footer.col.two.link.one"/></p>
-                <p><FormattedMessage id="page.home.footer.col.two.link.two"/></p>
-                <p><FormattedMessage id="page.home.footer.col.two.link.three"/></p>
+                <p className={styles.link}><FormattedMessage id="page.home.footer.col.two.link.one"/></p>
+                <p className={styles.link}><FormattedMessage id="page.home.footer.col.two.link.two"/></p>
+                <p className={styles.link}><FormattedMessage id="page.home.footer.col.two.link.three"/></p>
             </div>
             <div className={styles.colThree}>
-                <p><FormattedMessage id="page.home.footer.col.three.link.one"/></p>
-                <p><FormattedMessage id="page.home.footer.col.three.link.two"/></p>
-                <p><FormattedMessage id="page.home.footer.col.three.link.three"/></p>
+                <p className={styles.link}><FormattedMessage id="page.home.footer.col.three.link.one"/></p>
+                <p className={styles.link}><FormattedMessage id="page.home.footer.col.three.link.two"/></p>
+                <p className={styles.link}><FormattedMessage id="page.home.footer.col.three.link.three"/></p>
             </div>
             <div className={styles.colFour}>
-                <h4><FormattedMessage id="page.home.footer.col.four.header"/></h4>
-                <p><FormattedMessage id="page.home.footer.col.four.link.one"/></p>
-                <p><FormattedMessage id="page.home.footer.col.four.link.two"/></p>
-                <p><FormattedMessage id="page.home.footer.col.four.link.three"/></p>
-                <p><FormattedMessage id="page.home.footer.col.four.link.four"/></p>
-                <p><FormattedMessage id="page.home.footer.col.four.link.five"/></p>
+                <div className={styles.headerWithLineTwo}>
+                    <div className={styles.lineTwo}></div>
+                    <h4 className={styles.header}><FormattedMessage id="page.home.footer.col.four.header"/></h4>
+                </div>
+                <p className={styles.location}><FormattedMessage id="page.home.footer.col.four.link.one"/></p>
+                <p className={styles.location}><FormattedMessage id="page.home.footer.col.four.link.two"/></p>
+                <p className={styles.location}><FormattedMessage id="page.home.footer.col.four.link.three"/></p>
+                <p className={styles.location}><FormattedMessage id="page.home.footer.col.four.link.four"/></p>
+                <p className={styles.location}><FormattedMessage id="page.home.footer.col.four.link.five"/></p>
                 <div className={styles.smallText}>
                     <p><FormattedMessage id="page.home.footer.col.four.small.one"/></p>
                     <p><FormattedMessage id="page.home.footer.col.four.small.two"/></p>
